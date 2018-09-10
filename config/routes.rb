c482985ed754e7825f99b4e04f1b3f8978c17d6f
Jürgen Droste <Juergen.Droste@telefonica.com>
2018-09-10 15:46:08 +0200

    initila ruby project resources

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  resources :posts
+  root 'pages#home'
+
+  get 'pages/about'
+  get 'pages/home'
+  get 'pages/contact'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end

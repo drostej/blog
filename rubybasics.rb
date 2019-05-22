a76ccaf2ff211e7e77442f5a13c59876ea40fd38
Jürgen Droste <Juergen.Droste@telefonica.com>
2019-05-22 13:57:45 +0200

    commit hook und filewatch

--- rubybasics.rb
+++ rubybasics.rb
@@ -3,24 +3,21 @@ b = 123
 a_und_b = a + b.to_s
 puts a_und_b
 
-
 puts "--regular expression only characters and figures --"
 id = "cf87640f-77fb-4208-8e5f-bb7ea1bf59b0]"
 puts "id is ", id
 newid = id.match(/(?<id>[a-zA-Z0-9-]+)/)
-puts "New id", newid[:id ]
+puts "New id", newid[:id]
 
 puts "-- 3.times loop"
 3.times do
   puts "Hallo Welt"
 end
 
-
 puts "-- 15.upto(18)"
 15.upto(18) do |a_number|
   puts "Durchlauf Nr. #{a_number}"
 end
-
 
 puts "--string symbol test"
 puts "--string--"
@@ -35,9 +32,9 @@ puts string.count("l")
 
 puts "--array operations"
 puts "--array--"
-a = [1,2,4,6]
+a = [1, 2, 4, 6]
 puts a[3]
-a[1] = a[1]*2
+a[1] = a[1] * 2
 puts a[1]
 puts "--complete array"
 a << "Test"
@@ -47,8 +44,8 @@ puts a.reverse
 
 puts "--hash operations"
 puts "--hash--"
-h1 = { :one => "eins", two: "zwei" }
-h2 = { "three" => "drei", 4 => "vier" }
+h1 = {:one => "eins", two: "zwei"}
+h2 = {"three" => "drei", 4 => "vier"}
 puts h1[:two]
 h2[five: "fuenf"]
 h2.store(:six, "sechs")
